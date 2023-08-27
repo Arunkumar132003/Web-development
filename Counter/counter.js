fcbec3b2a633d9893457df5a25287c2cd59d3814
Arun Kumar G <96881025+Arunkumar132003@users.noreply.github.com>
2023-08-27 22:14:30 +0530

    Update counter.js

--- Counter/counter.js
+++ Counter/counter.js
@@ -4,14 +4,14 @@
             x+=1
             document.getElementById('count-display').innerText=x;
         }
+
         function dec(){
             x-=1;
             var t= document.getElementById('count-display').innerText=x;
-
         }
+
         function zero(){
             y=0;
             x=0;
-            z=0;
             document.getElementById('count-display').innerText=y;
         }
